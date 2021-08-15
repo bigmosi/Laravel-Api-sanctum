94253cdc77cf18899e937d80257648f01f956a56
kinyeramos2021 <kinyeramo@gmail.com>
2021-08-15 23:36:38 +0300

    authentication and controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,13 +16,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// public Route
 
-Route::resource('products', ProductController::class);
-
+// Route::resource('products', ProductController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/search/{name}', [ProductController::class,'search']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::post('/products', [ProductController::class, 'store']);
+// protected Route
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+ 
 
 
 
